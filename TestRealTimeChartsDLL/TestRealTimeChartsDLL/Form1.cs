c2413cf5c78dd834b6e650ba7101ec596cfdff87
drExplicit <x3rv1th@hotmail.com>
2016-10-28 11:13:43 +0100

    Add files via upload

---
+++ TestRealTimeChartsDLL/TestRealTimeChartsDLL/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+using chartUpdate;
+
+namespace TestRealTimeCharts
+{
+    public partial class Form1 : Form
+    {
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chartUpdater cUpdater = new chartUpdater(cpuChart);
+
+            var t1 = System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                cUpdater.getPerformanceCounters();
+            });
+        }
+    }
+}
---
+++ TestRealTimeChartsDLL/chartUpdater/chartUpdater.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace chartUpdate
+{
+    public class chartUpdater
+    {
+
+
+
+        public chartUpdater(Chart chartControl)
+        {
+            chart = chartControl;
+
+        }
+
+        private Chart chart { get; set; }
+        
+
+    
+        private double[] cpuArray = new double[60];
+
+        public void getPerformanceCounters()
+        {
+            var cpuPerfCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+            while (true)
+            {
+                cpuArray[cpuArray.Length - 1] = Math.Round(cpuPerfCounter.NextValue(), 0);
+
+                Array.Copy(cpuArray, 1, cpuArray, 0, cpuArray.Length - 1);
+
+                if (chart.IsHandleCreated)
+                {
+                    chart.Invoke((MethodInvoker)delegate { UpdateCpuChart(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                Thread.Sleep(1000);
+            }
+
+
+        }
+
+
+        private void UpdateCpuChart()
+        {
+            chart.Series["Series1"].Points.Clear();
+
+            for (int i = 0; i < cpuArray.Length - 1; ++i)
+            {
+                chart.Series["Series1"].Points.AddY(cpuArray[i]);
+            }
+        }
+
+
+
+
+    }
+}
